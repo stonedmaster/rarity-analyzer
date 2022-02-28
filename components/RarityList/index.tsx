4de5b1db505491b8e40fc5ece49ac0337feee1ec
Stoned Master <91418929+stonedmaster@users.noreply.github.com>
2022-02-28 22:27:26 +0200

    fix id

--- components/RarityList/index.tsx
+++ components/RarityList/index.tsx
@@ -83,9 +83,9 @@ const RarityList = ({
 							<input
 								type="text"
 								placeholder="Search ID"
-								value={tokenId}
+								value={tokenId - 1}
 								onKeyUp={(e: any) => e.key === "Enter" && openToken()}
-								onChange={(e) => setTokenId(e.target.value - 1)}
+								onChange={(e) => setTokenId(e.target.value)}
 								className="block w-full p-2 border border-gray-200 rounded form-input sm:text-sm sm:leading-5 bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-300 dark:border-gray-600"
 							/>
 						</div>
